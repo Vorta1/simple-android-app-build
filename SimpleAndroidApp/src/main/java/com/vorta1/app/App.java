5882339238561c1aa1c4024b133b98d24bf0e229
PeterFlower <peter.flower@tcsjohnhuxley.com>
2019-02-05 13:41:47 +0000

    initial commit

---
+++ SimpleAndroidApp/src/main/java/com/vorta1/app/App.java
@@ -0,0 +1,31 @@
+package com.vorta1.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class App extends Activity
+{
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+	}
+}
